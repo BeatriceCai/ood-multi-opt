b226f401a81dc731bb90fc583742765f76796c39
Tian Cai <33879882+BeatriceCai@users.noreply.github.com>
2022-03-30 21:55:31 -0400

    Add files via upload

---
+++ trainer.py
@@ -0,0 +1,83 @@
+import warnings
+warnings.filterwarnings('ignore')
+import pandas as pd
+import numpy as np
+import time
+import torch
+from sklearn.metrics import roc_auc_score
+import torch.nn.functional as F
+#------------------------
+from utils import *
+from models import *
+from ood_enhance_loss import *
+
+def trainer_alternative_1_loop(model, train_df,all_config,optimizer,scheduler):
+    loss_train, train_score, loss_ood,loss_pgi= [], [], [],[]
+    loss_fn = torch.nn.CrossEntropyLoss()
+    for outer_step in range(all_config['outer_step']):
+        batch_smiles, batch_target = batch_sample_multi(train_df, all_config)
+        # print(f'    outer step {outer_step}')
+        pred_logit = model(batch_smiles)
+        loss = loss_fn(pred_logit, batch_target)
+        loss_train.append(loss.item())
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        scheduler.step()
+
+    for ood_step in range(all_config['OOD_step']):
+        batch_smiles, batch_target = batch_sample_multi(train_df, all_config)
+        x = model.chem_embed(batch_smiles,OOD_enhence_step=True)
+        mul = model.cal_mul(x)
+        mode_set = mul.mean(0, keepdim=True)[0]
+        i = np.random.choice(all_config['endpoint_num'], 1)[0]
+
+        loss_ood_i, loss_pgi_i = get_ood_enhance_loss(i, mul, mode_set, all_config, model, x)
+
+        if all_config['pgi']==1:
+            loss_ood.append(loss_ood_i.item())
+            loss_pgi.append(loss_pgi_i.item())
+            optimizer.zero_grad()
+            loss = loss_ood_i + loss_pgi_i
+            loss.backward()
+            optimizer.step()
+            scheduler.step()
+        else:
+            loss_ood.append(loss_ood_i.item())
+            optimizer.zero_grad()
+            loss_ood_i.backward()
+            optimizer.step()
+            scheduler.step()
+
+
+    return loss_train, train_score,loss_ood,loss_pgi
+
+# def trainer_alternative_1_loop(model, train_df,all_config,optimizer,scheduler):
+#     loss_train, train_score, loss_ood = [], [], []
+#     loss_fn = torch.nn.CrossEntropyLoss()
+#     for outer_step in range(all_config['outer_step']):
+#         batch_smiles, batch_target = batch_sample_multi(train_df, all_config)
+#         # print(f'    outer step {outer_step}')
+#         pred_logit = model(batch_smiles)
+#         loss = loss_fn(pred_logit, batch_target)
+#         loss_train.append(loss.item())
+#         optimizer.zero_grad()
+#         loss.backward()
+#         optimizer.step()
+#         scheduler.step()
+#
+#     for ood_step in range(all_config['OOD_step']):
+#         batch_smiles, batch_target = batch_sample_multi(train_df, all_config)
+#         x = model.chem_embed(batch_smiles,OOD_enhence_step=True)
+#         mul = model.cal_mul(x)
+#         mode_set = mul.mean(0, keepdim=True)[0]
+#
+#         i = np.random.choice(all_config['endpoint_num'], 1)[0]
+#         loss = get_ood_enhance_loss(i, mul, mode_set, all_config['pick_OOD'], model, x)
+#
+#         loss_ood.append(loss.item())
+#         optimizer.zero_grad()
+#         loss.backward()
+#         optimizer.step()
+#         scheduler.step()
+#     return loss_train, train_score,loss_ood
---
+++ utils.py
@@ -0,0 +1,99 @@
+import os
+from datetime import datetime
+import json
+import pickle
+import warnings
+warnings.filterwarnings('ignore')
+import torch
+
+def set_up_exp_folder(all_config):
+    now = datetime.now()
+    timestamp = now.strftime("%d-%m-%Y-%H-%M-%S")
+    print('timestamp: ',timestamp)
+    save_folder = all_config['cwd'] + 'exp_log_'+all_config['exp_setting']+'/'
+    dataset_folder = save_folder +all_config['dataset']+'/'
+
+    if os.path.exists(save_folder) == False:
+            os.mkdir(save_folder)
+    if os.path.exists(dataset_folder) == False:
+            os.mkdir(dataset_folder)
+    checkpoint_dir = '{}exp{}/'.format(dataset_folder, timestamp)
+    if os.path.exists(checkpoint_dir ) == False:
+            os.mkdir(checkpoint_dir )
+    return checkpoint_dir
+
+##### JSON modules #####
+def save_json(data,filename):
+    with open(filename, 'w') as fp:
+        json.dump(data, fp, sort_keys=True, indent=4)
+
+def load_json(filename):
+    with open(filename, 'r') as fp:
+        data = json.load(fp)
+    return data
+
+##### pickle modules #####
+def save_dict_pickle(data,filename):
+    with open(filename,'wb') as handle:
+        pickle.dump(data,handle, pickle.HIGHEST_PROTOCOL)
+
+def load_pkl(path):
+    with open(path, 'rb') as f:
+        dict = pickle.load(f)
+    return  dict
+
+##### optimization modules #####
+def batch_sample(data, all_config,endpoint_list):
+    col = all_config['the_endpoint']
+    batch_data = data.groupby(col).sample(n=all_config['batch_size'],replace=True)
+
+    batch_smiles = batch_data['smiles'].values.tolist()
+    batch_target = torch.tensor(batch_data[all_config['the_endpoint']].values.tolist()).long()
+    return batch_smiles,batch_target
+
+def eval_fn(train_df,all_config,model,endpoint_list):
+    eval_logit = []
+    eval_target = []
+    for t in range(all_config['eval_step']):
+        if all_config['exp_setting']=='single_task':
+            batch_smiles, batch_target = batch_sample(train_df, all_config,endpoint_list)
+        elif all_config['exp_setting'] in ['multi_task_baseline' ,'multi_task_ood','multi_task_share_till_logit']:
+            head_config=all_config['head_config']
+            batch = train_df.groupby(['endpoint', 'y']).sample(n=all_config['batch_size'],replace=True)
+            batch_smiles = batch['similes'].values
+            batch_y = batch['y'].values
+            batch_target = torch.tensor(batch_y.reshape((head_config['channel_size'],
+                                                         head_config['batch_size'] * head_config[
+                                                             'output_size'])).swapaxes(0, 1)).long()
+            if all_config['use_cuda'] == 1 and torch.cuda.is_available():
+                batch_target=batch_target.to('cuda')
+        elif all_config['exp_setting']=='multi_task_all_shared':
+            head_config = all_config['head_config']
+            batch = train_df.groupby(['endpoint', 'y']).sample(n=all_config['batch_size'],replace=True)
+            batch_smiles = batch['similes'].values
+            batch_y = batch['y'].values
+            batch_target = torch.tensor(batch_y).reshape((head_config['batch_size']*head_config['output_size'],
+                                            head_config['channel_size']))
+        pred_logit = model(batch_smiles)
+        if all_config['exp_setting'] == 'multi_task_all_shared':
+            pred_logit = pred_logit.reshape((head_config['batch_size'] * head_config['output_size'],
+                                             head_config['channel_size'],2))
+            pred = list(pred_logit.detach().cpu().numpy()[:, :,1])
+        else:
+            pred = list(pred_logit.detach().cpu().numpy()[:, 1])
+        target = list(batch_target.detach().cpu().numpy())
+        eval_logit.extend(pred)
+        eval_target.extend(target)
+    return eval_logit,eval_target
+
+def batch_sample_multi(train_df,all_config):
+    head_config = all_config['head_config']
+    batch = train_df.groupby(['endpoint', 'y']).sample(n=all_config['batch_size'],replace=True)
+    batch_smiles = batch['similes'].values
+    batch_y = batch['y'].values
+    batch_target = torch.tensor(batch_y.reshape((head_config['channel_size'],
+                                                 head_config['batch_size'] * head_config['output_size'])).swapaxes(0,
+                                                                                                                   1)).long()
+    if all_config['use_cuda'] == 1 and torch.cuda.is_available():
+        batch_target = batch_target.to('cuda')
+    return batch_smiles,batch_target
